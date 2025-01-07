a869290ae3fb9feb8d628f10c32c000d89d51d14
Kozak Liubomyr <liubomyr.kozak@gmail.com>
2025-01-07 14:37:04 +0200

    add enums

---
+++ src/enums/SettingsEnum.ts
@@ -0,0 +1,10 @@
+export enum EWordPopupType {
+    CLICK = 'click',
+    AUTO = 'auto',
+}
+
+
+export enum EWordInteractType {
+    CLICK = 'click',
+    HOVER = 'hover',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export * from './UserWord.types';
-export * from './ApiTypes.types';
-export * from './ExtensionSettings.types';
+// TYPES
+export * from './types/UserWord.types';
+export * from './types/ApiTypes.types';
+export * from './types/ExtensionSettings.types';
+
+// ENUMS
+export * from './enums/SettingsEnum';
