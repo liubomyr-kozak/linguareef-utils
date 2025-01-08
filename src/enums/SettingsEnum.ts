3a6cf94638dd206f7d80fb81d75bc856e94706e4
Kozak Liubomyr <liubomyr.kozak@gmail.com>
2025-01-08 13:54:13 +0200

    test ETranslateType

--- src/enums/SettingsEnum.ts
+++ src/enums/SettingsEnum.ts
@@ -16,3 +16,19 @@ export enum ETranslateType {
     MIXED_TYPES = 3,
     DEFAULT = 0,
 }
+
+export enum ETranslateType2 {
+    MAIN = 'MAIN',
+    SOUND = 'SOUND',
+    DISPLAY_TRANSLATED_WORD = 'DISPLAY_TRANSLATED_WORD',
+    MIXED_TYPES = 'MIXED_TYPES',
+    DEFAULT = 'DEFAULT',
+}
+
+export enum ETranslateTypeNum {
+    MAIN = 0,
+    SOUND = 1,
+    DISPLAY_TRANSLATED_WORD = 2,
+    MIXED_TYPES = 3,
+    DEFAULT = 0,
+}
