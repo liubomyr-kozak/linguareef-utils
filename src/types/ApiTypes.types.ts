3c791aee3152d2ebc2572db124e91eaa763d4664
Kozak Liubomyr <liubomyr.kozak@gmail.com>
2025-01-09 03:06:27 +0200

    update API

--- src/types/ApiTypes.types.ts
+++ src/types/ApiTypes.types.ts
@@ -1,14 +1,20 @@
 import {IUserWord} from "./UserWord.types";
 import {IExtensionSettings} from "./ExtensionSettings.types";
 import {IUserVocabSelection} from "./UserVocab.types";
+import {IUserVocabWord} from "./UserVocabsWord.types";
 
 export type UserVocabularyDTO = {
     list: IUserWord[];
 }
 
+// /api/user/extension-settings
 export type UserExtensionSettingsDTO = IExtensionSettings
 
-export type UserSelectedVocabsWordsDTO = IUserVocabSelection[]
+// /api/vocabulary/user/selected-vocabs?langPair=en_uk
+export type UserVocabSelectionDTO = IUserVocabSelection[]
+
+// /api/vocabulary/user/selected-vocabs-words?langPair=en_uk
+export type UserSelectedVocabsWordsDTO = IUserVocabWord[]
 
 // /api/vocabulary/libraries/vocabs/current-user?langPair=en_uk
 // export type UserVocabularyLibraryExtensionDTO = {
@@ -16,4 +22,3 @@ export type UserSelectedVocabsWordsDTO = IUserVocabSelection[]
 // }
 
 
-// /api/vocabulary/user/selected-vocabs-words?langPair=en_uk
